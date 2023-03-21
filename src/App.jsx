c516a2de086445722bf1e98e50ddff3c443a6401
ashleystringer <ashleylstringer@gmail.com>
2023-03-21 19:24:58 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Portfolio from './Portfolio/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Portfolio/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar';
+import Box from '../Box/Box';
+
+export default function Portfolio() {
+    return (
+        <div>
+            <Navbar/>
+            <br/>
+            <Box/>
+            <br/>
+            <Box/>
+            <br/>
+            <Box/>
+      </div>
+    )
+}
